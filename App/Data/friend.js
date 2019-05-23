abc57b717da1ae29e20a0a3b38b96c761abed0d6
csporto23 <Csporto23@gmail.com>
2019-05-23 18:50:50 -0500

    commit

---
+++ App/Data/friend.js
@@ -0,0 +1,20 @@
+var friends = [
+    {
+       name: "Kevin",
+       photo: "https://www.drawingtutorials101.com/drawing-tutorials/Cartoon-Movies/Minions/kevin/how-to-draw-kevin-from-minions.jpg",
+       scores: [
+           5,
+           2,
+           4,
+           4,
+           2,
+           3,
+           4,
+           5,
+           2,
+           1
+       ] 
+    }
+]
+
+module.exports = friends
