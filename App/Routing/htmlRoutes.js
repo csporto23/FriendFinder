8aeb205a442ad3833d332e207688069b94755f0c
csporto23 <Csporto23@gmail.com>
2019-06-06 02:02:41 -0500

    trying to fix heroku deploy

--- App/Routing/htmlRoutes.js
+++ App/Routing/htmlRoutes.js
@@ -3,10 +3,10 @@ var path = require("path");
 module.exports = function(app) {
 //routes for html pages
 app.get("/", function(request, response) {
-    response.sendFile(path.join(__dirname, "../Public/home.html"));
+    response.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
 app.get("/survey", function(request, response) {
-    response.sendFile(path.join(__dirname, "../Public/survey.html"));
+    response.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 };
--- Server.js
+++ Server.js
@@ -1,10 +1,7 @@
 var express = require("express");
-const fs = require("fs");
 
-// Sets up the Express App
-// =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 
 // Sets up the Express app to handle data parsing
@@ -12,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-require("./app/Routing/htmlRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 //Listen for Server Port
 app.listen(PORT, function() {
